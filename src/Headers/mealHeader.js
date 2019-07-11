c7e7253c395fe1983efae620a00baac9f109ac4b
Ben <ben@ben.kleinschmidt.com>
2019-07-11 16:28:39 -0500

    put in some icons

--- src/Headers/mealHeader.js
+++ src/Headers/mealHeader.js
@@ -4,14 +4,10 @@ function MealHeader() {
   return (
   	<div className='row'>
       <span className='col-sm column-header'>Title</span>
-      <span className='col-sm column-header'>Day</span>
-      <span className='col-sm column-header'>Date</span>
       <span className='col-sm column-header'>Time</span>
       <span className='col-sm column-header'>Total Calories</span>
       <span className='col-sm column-header'>Health Rating</span>
-      <span className='col-sm column-header'>Foods</span>
-      <span className='col-sm column-header'>Save Meal</span>
-      <span className='col-sm column-header'>Remove Meal</span>
+      <span className='col-sm column-header'>&nbsp;</span>
     </div>
   );
 }
